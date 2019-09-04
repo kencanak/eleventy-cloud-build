39389dd73b2ece34f3594ba19e191571bff0e13b
Kencana <kencana@C02XKBGLJG5H.local>
2019-09-04 16:18:55 +0800

    chores: another debugging stuff

--- firebase.bash
+++ firebase.bash
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-# run the original firebase
-if [ $FIREBASE_TOKEN ]; then
-  firebase deploy --only hosting --token $FIREBASE_TOKEN
-else
-	echo "Hello, world! The time is $(date)."
-  firebase deploy --only hosting
-fi
+firebase deploy --only hosting
