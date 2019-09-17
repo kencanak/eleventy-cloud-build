49270d67aa707ae03d66c12e8950566ccd0fdb72
Kencana <kencana@C02XKBGLJG5H.local>
2019-09-17 15:59:39 +0800

    chores: add request raw body into console log

--- functions/index.js
+++ functions/index.js
@@ -5,12 +5,19 @@ const functions = require('firebase-functions');
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
 	console.log(request.body);
-	const event_type = request.headers["X-GitHub-Event"];
+	const event_type = request.headers['x-gitHub-event'];
 	const payload = JSON.parse(request.body.payload);
 
-	console.log('HEADERS');
-	console.log(request.headers);
-	console.log(`EVENT TYPE: ${event_type}`);
-	console.log(payload);
- 	response.send("Hello from Firebase!");
+	if (event_type === 'pull_request') {
+		console.log('HEADERS');
+		console.log(request.headers);
+		console.log(`EVENT TYPE: ${event_type}`);
+		console.log(payload);
+
+		console.log('RAW BODY:');
+		console.log(request.rawBody);
+		response.send("Hello from Firebase!");
+	} else {
+		response.status(403).send('forbidden request');
+	}
 });
